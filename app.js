7a3a4ef175494dab39d46ee05d6b18eddef47f1c
Ubaiduulah <ubaidullah5150@gmail.com>
2021-11-15 20:29:54 +0500

    work done

---
+++ app.js
@@ -0,0 +1,52 @@
+//alert("hello world")
+//      chap 31-34 :
+//      ques 1:
+//var datetime = new Date();
+//document.write("" + datetime);
+//      ques 2 :
+//document.write("current month: December")
+//alert("current month: December")
+//      ques 3 :
+//document.write("current day: Sun")
+//alert("current week: Sun")
+//      ques 4:
+//var day = prompt ("Enter today Days")
+//if( day == "sunday" ){
+//alert("It's a Fun Day!")
+//}
+//else{
+// alert("Its a working day")
+//}
+//    ques 5:
+//var day = +prompt ("Enter the date of the month");
+//if( day = 1 < 15 ){
+//alert("First fifteen days of the month");
+//}
+//else if( day = 16 < 30 ){
+ //   alert("Last days of the month");
+//}
+//else{
+// alert("Enter a valid day")
+//}
+//      ques 13
+//var age = +prompt("Enter your age");
+//var year = +prompt("Enter current year")
+//var days = year - age
+//document.write("Your age is " + age + "<br>" )
+//document.write("Your birth month is " + days )
+//     ques 14
+/*var name = prompt("Enter your Name");
+var month = prompt("Enter Current Month");
+var unit = +prompt("Enter your Units");
+var perunit = +prompt("Enter per Unit Charge");
+var surcharge = 350;
+var AmountPayable = unit *  perunit 
+var GrossAmountPayable = AmountPayable + surcharge 
+document.write("K-ELECTRIC BILL " + "<br>" )
+document.write("Customer name: " + name + "<br>" )
+document.write("Month: " + month + "<br>" )
+document.write("Number of unit:  " + unit + "<br>" )
+document.write("Charge per unit : " + perunit + "<br>" )
+document.write("Net Amount Payable (within Due Date): " + AmountPayable + "<br>" )
+document.write("SurgeCharge:" + surcharge + "<br>" )
+document.write("Gross Amount Payable (after Due Date): " + GrossAmountPayable + "<br>" )
